033d99ca6eacbcb5c084c4997dd7f29ba7ac1558
ValeriaJahzeel <chj068378@gmail.com>
2023-04-29 13:08:18 -0600

    tarea9 v1.0

---
+++ ejemplos/ejemplo2/javita.js
@@ -0,0 +1,6 @@
+function cliquear(){
+    console.log("le puchaste xd");
+}
+const boton=document.querySelector("#boton");
+//Se pone entre comillas la accion
+boton.addEventListener("click", cliquear);
